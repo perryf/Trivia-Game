cedcbed1cbcf478f12cad65e7686671be8ebb010
Steven Connors <steven.connors53@gmail.com>
2017-10-25 14:11:35 -0400

    mvp is done and working. Time to move on to bronze level objectives

--- script.js
+++ script.js
@@ -4,28 +4,53 @@ $(document).ready(function(){
       question:'something here',
       choices:['first choice here', 'second choice here', 'third choice here', 'fourth choice here'],
       answer:'first choice here'
+    },
+    {
+      question:'test for question 2',
+      choices:['test choice 1', 'test choice 2'],
+      answer:'test choice 2'
     }
   ]
-
+  var totalCorrect = 0
+  var totalWrong = 0
+  var allChoicesHtml = ""
   var currentIndex = 0
   var currentQuestion
 
   function startGame () {
+    var totalCorrect = 0
+    var totalWrong = 0
+    var allChoicesHtml = ""
+    var currentIndex = 0
+    var currentQuestion
+    const trivia = [
+      {
+        question:'something here',
+        choices:['first choice here', 'second choice here', 'third choice here', 'fourth choice here'],
+        answer:'first choice here'
+      },
+      {
+        question:'test for question 2',
+        choices:['test choice 1', 'test choice 2'],
+        answer:'test choice 2'
+      }
+    ]
     $('.start-menu').hide()
     selectQuestion()
 
   }
 
   function selectQuestion () {
+    allChoicesHtml = ""
     currentQuestion = trivia[currentIndex]
-    displayQuestion()
     currentIndex++
+    displayQuestion()
   }
 
   function displayQuestion() {
     let questionText = currentQuestion.question
     questionHtml = "<p class = 'question'>" + questionText + "</p>"
-    $('.game').append(questionHtml)
+    // $('.game').html(questionHtml)
     displayChoices()
     $('.game').show()
 
@@ -35,12 +60,14 @@ $(document).ready(function(){
 
   function displayChoices() {
     let choicesText = currentQuestion.choices
-    for (i = 0; i<choicesText.length; i++){
+    for (i = 0; i < choicesText.length; i++){
       choicesHtml = "<p class='choices' data-choice='" + i + "'>" + choicesText[i] + "</p>"
-      $('.game').append(choicesHtml)
+      allChoicesHtml += choicesHtml
+      // $('.game').append(choicesHtml)
     }
+    showAll ="<div class = gameBoard> "+questionHtml+allChoicesHtml+"</div>"
+    $('.game').html(showAll)
     userSelection()
-
   }
   function userSelection() {
     $('.choices').click(function( e ){
@@ -49,14 +76,65 @@ $(document).ready(function(){
       parseInt(userAnswer)
       //parseInt(target)
       if (choicesText[userAnswer] === currentQuestion.answer){
-        //correctAnswer()
+        correctAnswer()
+        $('.gameBoard').remove()
+        totalCorrect++
+        console.log(totalCorrect)
       }
       else {
-        //incorrectAnswer()
+        incorrectAnswer()
+        $('p').remove()
+        totalWrong++
+        console.log(totalWrong)
       }
       // follow up: research jQuery methods for getting data attributes
     })
   }
+  function correctAnswer(){
+    $('.game').hide()
+    correctImageHtml = "<img src='check.jpg' alt = 'Correct'>"
+    $('.rightOrWrong').html(correctImageHtml)
+    $('.rightOrWrong').show()
+
+    nextQuestion()
+  }
+
+  function incorrectAnswer() {
+    $('.game').hide()
+    incorrectImageHtml = "<img src='wrong.png' alt = 'Incorrect'>"
+    $('.rightOrWrong').html(incorrectImageHtml)
+    $('.rightOrWrong').show()
+    nextQuestion()
+  }
+  function nextQuestion() {
+    setTimeout(checkQuestions, 3000)
+  }
+  function checkQuestions() {
+    $('.rightOrWrong').hide()
+
+    console.log(currentIndex)
+    if (currentIndex < trivia.length){
+      selectQuestion()
+    }
+    else {
+      endGame()
+    }
+  }
+  function endGame() {
+    endScreenHtml = "<p class='ending'> You answered " + totalCorrect + " questions correctly and " + totalWrong + " incorrectly </p> "
+    // <button type='button' id = 'reset-button'> Reset</button>
+
+    $('.end').html(endScreenHtml)
+    $('.end').show()
+  //  $('#reset-button').click(startGame)
+  }
+    // setTimeout for 3000
+    // callback fn
+    //   1. hide rightOrWrong
+    //   2. Check currentIndex and questions.length
+    //     A. call displayQuestion
+    //     B. End Game
   var choicesHtml
   $('#start-button').click(startGame)
+  $('#reset-button').click(startGame)
 })
